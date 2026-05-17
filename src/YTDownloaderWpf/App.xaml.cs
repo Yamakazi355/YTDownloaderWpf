ef68c3856b2ba1e3f30271b9842e175eac6a2a31
Yamakazi355 <kuba2000104@gmail.com>
2026-05-17 18:50:03 +0200

    Initial WPF project

---
+++ src/YTDownloaderWpf/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace YTDownloaderWpf;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
